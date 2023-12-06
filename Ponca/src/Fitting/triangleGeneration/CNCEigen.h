2a95571f4e189d26865f216bd92390eef25b5869
Leo <arnalleouis@gmail.com>
2023-12-06 10:24:23 +0100

    using Scalar and not only double to CNC

--- Ponca/src/Fitting/triangleGeneration/CNCEigen.h
+++ Ponca/src/Fitting/triangleGeneration/CNCEigen.h
@@ -1,21 +1,23 @@
 #include <Eigen/Dense>
 
+template <typename _Scalar>
 struct CNCEigen {
 
+  using Scalar = _Scalar;
   /// Small constant used to approximate zero.
-  static constexpr double epsilon = 1e-8;
+  static constexpr Scalar epsilon = 1e-8;
 
   /// Represents a triangle on a sphere of radius one.
   struct SphericalTriangle {
     
     ///Spherical point data type
-    typedef Eigen::Vector3d Vector3;
+    typedef Eigen::Vector3<Scalar, 3> Vector3;
     
     static
     bool
     isDegenerate(const Vector3 &a, const Vector3 &b, const Vector3 &c)
     {
-      double d[ 3 ] = { ( a - b ).norm(),( a - c ).norm(),( b - c ).norm() };
+      Scalar d[ 3 ] = { ( a - b ).norm(),( a - c ).norm(),( b - c ).norm() };
       // Checks that the spherical triangle is small or thin.
       if ( ( d[ 0 ] < epsilon ) || ( d[ 1 ] < epsilon ) || ( d[ 2 ] < epsilon ) )
         return true;
@@ -48,7 +50,7 @@ struct CNCEigen {
     static
     void
     interiorAngles( const Vector3 &a, const Vector3 &b, const Vector3 &c,
-		    double& alpha, double& beta, double& gamma )
+		    Scalar& alpha, Scalar& beta, Scalar& gamma )
     {
       Vector3 Ta,Tb,Tc;
       polarTriangle(a,b,c,Ta,Tb,Tc);
@@ -59,9 +61,9 @@ struct CNCEigen {
         alpha = beta = gamma = 0.0;
       else
       {
-        double ca = std::max( -1.0, std::min( 1.0, Tb.dot( Tc ) ) );
-        double cb = std::max( -1.0, std::min( 1.0, Tc.dot( Ta ) ) );
-        double cc = std::max( -1.0, std::min( 1.0, Ta.dot( Tb ) ) );
+        Scalar ca = std::max( -1.0, std::min( 1.0, Tb.dot( Tc ) ) );
+        Scalar cb = std::max( -1.0, std::min( 1.0, Tc.dot( Ta ) ) );
+        Scalar cc = std::max( -1.0, std::min( 1.0, Ta.dot( Tb ) ) );
         alpha     = acos( ca );
         beta      = acos( cb );
         gamma     = acos( cc );
@@ -70,10 +72,10 @@ struct CNCEigen {
     
     /// @return the (unsigned) area of the spherical triangle (below 2pi).
     static
-    double
+    Scalar
     area(const Vector3 &a, const Vector3 &b, const Vector3 &c)
     {
-      double alpha, beta, gamma;
+      Scalar alpha, beta, gamma;
       if ( isDegenerate(a,b,c) ) return 0.0;
       interiorAngles( a, b, c, alpha, beta, gamma );
       return ( ( fabs(alpha) < epsilon )
@@ -85,10 +87,10 @@ struct CNCEigen {
     
     /// @return the (signed) area of the spherical triangle (below 2pi).
     static
-    double
+    Scalar
     algebraicArea(const Vector3 &a, const Vector3 &b, const Vector3 &c) 
     {
-      double  S = area(a,b,c);
+      Scalar  S = area(a,b,c);
       Vector3 M = a + b + c;
       Vector3 X = ( b - a ).cross( c - a );
       if ( M.lpNorm<1>() <= epsilon || X.lpNorm<1>() <= epsilon ) return 0.0;
@@ -115,18 +117,18 @@ public:
   /// interpolated corrected normals may have smaller norms.
   /// @return the mu0-measure of triangle abc, i.e. its area.
   static
-  double
-  mu0InterpolatedU( const Eigen::Vector3d & a,
-		    const Eigen::Vector3d & b,
-		    const Eigen::Vector3d & c,
-		    const Eigen::Vector3d & ua,
-		    const Eigen::Vector3d & ub,
-		    const Eigen::Vector3d & uc,
+  Scalar
+  mu0InterpolatedU( const Eigen::Vector<Scalar, 3> & a,
+		    const Eigen::Vector<Scalar, 3> & b,
+		    const Eigen::Vector<Scalar, 3> & c,
+		    const Eigen::Vector<Scalar, 3> & ua,
+		    const Eigen::Vector<Scalar, 3> & ub,
+		    const Eigen::Vector<Scalar, 3> & uc,
 		    bool unit_u = false)
   {
     // MU0=1/2*det( uM, B-A, C-A )
     //    =  1/2 < ( (u_A + u_B + u_C)/3.0 ) | (AB x AC ) >
-    Eigen::Vector3d uM = ( ua+ub+uc ) / 3.0;
+    Eigen::Vector<Scalar, 3> uM = ( ua+ub+uc ) / 3.0;
     if ( unit_u )
     {
       auto uM_norm = uM.norm();
@@ -149,17 +151,17 @@ public:
   /// interpolated corrected normals may have smaller norms.
   /// @return the mu1-measure of triangle abc, i.e. its mean curvature.
   static
-  double
-  mu1InterpolatedU( const Eigen::Vector3d& a,
-		    const Eigen::Vector3d & b,
-		    const Eigen::Vector3d& c,
-		    const Eigen::Vector3d& ua,
-		    const Eigen::Vector3d& ub,
-		    const Eigen::Vector3d& uc,
+  Scalar
+  mu1InterpolatedU( const Eigen::Vector<Scalar, 3>& a,
+		    const Eigen::Vector<Scalar, 3> & b,
+		    const Eigen::Vector<Scalar, 3>& c,
+		    const Eigen::Vector<Scalar, 3>& ua,
+		    const Eigen::Vector<Scalar, 3>& ub,
+		    const Eigen::Vector<Scalar, 3>& uc,
 		    bool unit_u = false)
   {
     // MU1=1/2( | uM u_C-u_B A | + | uM u_A-u_C B | + | uM u_B-u_A C |
-    Eigen::Vector3d uM = ( ua+ub+uc ) / 3.0;
+    Eigen::Vector<Scalar, 3> uM = ( ua+ub+uc ) / 3.0;
     if ( unit_u )  uM /= uM.norm();
     return 0.25 * ( uM.cross( uc - ub ).dot( a )
                    + uM.cross( ua - uc ).dot( b )
@@ -180,13 +182,13 @@ public:
   /// interpolated corrected normals may have smaller norms.
   /// @return the mu2-measure of triangle abc, i.e. its Gaussian curvature.
   static
-  double
-  mu2InterpolatedU( const Eigen::Vector3d& a,
-		    const Eigen::Vector3d& b,
-		    const Eigen::Vector3d& c,
-		    const Eigen::Vector3d& ua,
-		    const Eigen::Vector3d& ub,
-		    const Eigen::Vector3d& uc,
+  Scalar
+  mu2InterpolatedU( const Eigen::Vector<Scalar, 3>& a,
+		    const Eigen::Vector<Scalar, 3>& b,
+		    const Eigen::Vector<Scalar, 3>& c,
+		    const Eigen::Vector<Scalar, 3>& ua,
+		    const Eigen::Vector<Scalar, 3>& ub,
+		    const Eigen::Vector<Scalar, 3>& uc,
 		    bool unit_u = false )
   {
     // Using non unitary interpolated normals give
@@ -209,31 +211,31 @@ public:
   /// @param uc the corrected normal vector at point c
   /// @return the muXY-measure of triangle abc, i.e. its anisotropic curvature.
   static
-  Eigen::Matrix3d
-  muXYInterpolatedU( const Eigen::Vector3d& a,
-		     const Eigen::Vector3d& b,
-		     const Eigen::Vector3d& c,
-		     const Eigen::Vector3d& ua,
-		     const Eigen::Vector3d& ub,
-		     const Eigen::Vector3d& uc,
+  Eigen::Matrix<Scalar, 3, 3>
+  muXYInterpolatedU( const Eigen::Vector<Scalar, 3>& a,
+		     const Eigen::Vector<Scalar, 3>& b,
+		     const Eigen::Vector<Scalar, 3>& c,
+		     const Eigen::Vector<Scalar, 3>& ua,
+		     const Eigen::Vector<Scalar, 3>& ub,
+		     const Eigen::Vector<Scalar, 3>& uc,
 		     bool unit_u = false )
   {
-    Eigen::Matrix3d  T = Eigen::Matrix3d::Zero();
-    Eigen::Vector3d uM = ( ua+ub+uc ) / 3.0;
+    Eigen::Matrix<Scalar, 3, 3>  T = Eigen::Matrix<Scalar, 3, 3>::Zero();
+    Eigen::Vector<Scalar, 3> uM = ( ua+ub+uc ) / 3.0;
     if ( unit_u )  uM /= uM.norm();
-    const Eigen::Vector3d uac = uc - ua;
-    const Eigen::Vector3d uab = ub - ua;
-    const Eigen::Vector3d  ab = b - a;
-    const Eigen::Vector3d  ac = c - a;
+    const Eigen::Vector<Scalar, 3> uac = uc - ua;
+    const Eigen::Vector<Scalar, 3> uab = ub - ua;
+    const Eigen::Vector<Scalar, 3>  ab = b - a;
+    const Eigen::Vector<Scalar, 3>  ac = c - a;
     for ( size_t i = 0; i < 3; ++i )
     {
-      Eigen::Vector3d X = Eigen::Vector3d::Zero();
+      Eigen::Vector<Scalar, 3> X = Eigen::Vector<Scalar, 3>::Zero();
       X(i) = 1.0 ;
       for ( size_t j = 0; j < 3; ++j )
       {
         // Since RealVector Y = RealVector::base( j, 1.0 );
         // < Y | uac > = uac[ j ]
-        const double tij =
+        const Scalar tij =
 	  0.5 * uM.dot( uac[ j ] * X.cross( ab )
 			- uab[ j ] * X.cross( ac ) );
         T(i,j) = tij;
@@ -255,31 +257,31 @@ public:
   /// @param N the normal vector
   /// @return a pair of principal directions.
   static
-  std::pair<Eigen::Vector3d, Eigen::Vector3d>
-  curvDirFromTensor( const Eigen::Matrix3d& tensor,
-		     const double           area,
-		     const Eigen::Vector3d& N )
+  std::pair<Eigen::Vector<Scalar, 3>, Eigen::Vector<Scalar, 3>>
+  curvDirFromTensor( const Eigen::Matrix<Scalar, 3, 3>& tensor,
+		     const Scalar           area,
+		     const Eigen::Vector<Scalar, 3>& N )
   {
     auto Mt = tensor.transpose();
     auto M  = tensor;
     M      += Mt;
     M      *= 0.5;
-    const double coef_N = 1000.0 * area;
+    const Scalar coef_N = 1000.0 * area;
     // Adding 1000 area n x n to anisotropic measure
     for ( int j = 0; j < 3; j++ )
       for ( int k = 0; k < 3; k++ )
         M( j, k ) += coef_N * N[ j ] * N[ k ];
     
-    Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> eigensolver(M);
+    Eigen::SelfAdjointEigenSolver<Eigen::Matrix<Scalar, 3, 3>> eigensolver(M);
     if (eigensolver.info() != Eigen::Success) abort();
     
     //SelfAdjointEigenSolver returns sorted eigenvalues, no
     //need to reorder the eigenvectors.
     assert(eigensolver.eigenvalues()(0) <= eigensolver.eigenvalues()(1) );
     assert(eigensolver.eigenvalues()(1) <= eigensolver.eigenvalues()(2) );    
-    Eigen::Vector3d v1 = eigensolver.eigenvectors().col(1);
-    Eigen::Vector3d v2 = eigensolver.eigenvectors().col(0);
-    return std::pair<Eigen::Vector3d,Eigen::Vector3d>(v1,v2);
+    Eigen::Vector<Scalar, 3> v1 = eigensolver.eigenvectors().col(1);
+    Eigen::Vector<Scalar, 3> v2 = eigensolver.eigenvectors().col(0);
+    return std::pair<Eigen::Vector<Scalar, 3>,Eigen::Vector<Scalar, 3>>(v1,v2);
   }
 
   /// Computing principal curvatures k1 and k2 from tensor
@@ -288,30 +290,30 @@ public:
   /// @param N the normal vector
   /// @return a pair of principal directions.
   static
-  std::tuple< double, double, Eigen::Vector3d, Eigen::Vector3d >
-  curvaturesFromTensor( const Eigen::Matrix3d &tensor,
-			const double           area,
-			const Eigen::Vector3d &N )
+  std::tuple< Scalar, Scalar, Eigen::Vector<Scalar, 3>, Eigen::Vector<Scalar, 3> >
+  curvaturesFromTensor( const Eigen::Matrix<Scalar, 3, 3> &tensor,
+			const Scalar           area,
+			const Eigen::Vector<Scalar, 3> &N )
   {
     auto Mt = tensor.transpose();
     auto M  = tensor;
     M      += Mt;
     M      *= 0.5;
-    const double coef_N = 1000.0 * area;
+    const Scalar coef_N = 1000.0 * area;
     // Adding 1000 area n x n to anisotropic measure
     for ( int j = 0; j < 3; j++ )
       for ( int k = 0; k < 3; k++ )
         M( j, k ) += coef_N * N[ j ] * N[ k ];
     
-    Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> eigensolver(M);
+    Eigen::SelfAdjointEigenSolver<Eigen::Matrix<Scalar, 3, 3>> eigensolver(M);
     if (eigensolver.info() == Eigen::Success)
       {
 	// SelfAdjointEigenSolver returns sorted eigenvalues, no
 	// need to reorder the eigenvectors.
 	assert(eigensolver.eigenvalues()(0) <= eigensolver.eigenvalues()(1) );
 	assert(eigensolver.eigenvalues()(1) <= eigensolver.eigenvalues()(2) );    
-	Eigen::Vector3d v1 = eigensolver.eigenvectors().col(0);
-	Eigen::Vector3d v2 = eigensolver.eigenvectors().col(1);
+	Eigen::Vector<Scalar, 3> v1 = eigensolver.eigenvectors().col(0);
+	Eigen::Vector<Scalar, 3> v2 = eigensolver.eigenvectors().col(1);
 	return std::make_tuple( -eigensolver.eigenvalues()(0),
 				-eigensolver.eigenvalues()(1),
 				v1,v2 );
@@ -319,7 +321,7 @@ public:
     else
       {
 	std::cerr << "Incorrect diagonalization for tensor " << M << std::endl;
-	Eigen::Vector3d v1, v2;
+	Eigen::Vector<Scalar, 3> v1, v2;
 	return std::make_tuple( 0.0, 0.0, v1,v2 );
       }
   }

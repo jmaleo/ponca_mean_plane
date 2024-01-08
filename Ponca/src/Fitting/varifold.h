89db71fab79a3e4911b0ec2724e60e0f0ccfcbf0
Leo <arnalleouis@gmail.com>
2024-01-08 18:06:38 +0100

    Add varifolds

---
+++ Ponca/src/Fitting/varifold.h
@@ -0,0 +1,218 @@
+#pragma once
+
+#include <Ponca/Fitting>
+#include <Eigen/Eigenvalues>
+#include "meanPlaneFit.h"
+
+// TODO
+// - inherit from plane primitive
+// - inherit from curvature class
+// - remove v0
+
+namespace Ponca
+{
+
+// forward declarations
+namespace v0 {
+    template<class D, class W, typename T>
+    class Varifold;
+    // weight kernel not really used (see comments below)
+    template<class S>
+    using VarifoldWeightKernel = ConstantWeightKernel<S>; 
+}
+namespace v1 {
+    template<class D, class W, typename T>
+    class Varifold;
+    template<class S>
+    class VarifoldWeightKernel;
+}
+
+// ============================================================================
+
+// 
+// use version 0 or 1
+//
+// v0: stores a 3x3x3 tensor (close to article's notations)
+// v1: stores a 3x3 matrix
+// 
+template<class D, class W, typename T>
+using Varifold = v1::Varifold<D,W,T>;
+template<class S>
+using VarifoldWeightKernel = v1::VarifoldWeightKernel<S>;
+
+// ============================================================================
+
+namespace v0 
+{
+
+//! 
+//! Implementation of 
+//!     Weak and approximate curvatures of a measure: a varifold perspective
+//!     B Buet, GP Leonardi, S Masnou
+//!     
+//! See Equation 7.2 of the article
+//!
+//! The _WFunctor is not used except for the centered basis and the scale.
+//! Weights values of the _WFunctor are not used.
+//! 
+//! Remarks
+//! - point weights (m_l) are set to 1
+//! - only valid in 3D
+//! - normals are required
+//! 
+template<class DataPoint, class _WFunctor, typename T>
+class Varifold : public T
+{
+    PONCA_FITTING_DECLARE_DEFAULT_TYPES
+    PONCA_FITTING_DECLARE_MATRIX_TYPE
+
+protected:
+    using Mat32 = Eigen::Matrix<Scalar,3,2>;
+    using Mat22 = Eigen::Matrix<Scalar,2,2>;
+
+    static_assert(DataPoint::Dim == 3); // only valid in 3D
+
+    enum 
+    {
+        Check = Base::PROVIDES_PRIMITIVE_BASE &&
+                Base::PROVIDES_MEAN_NORMAL &&
+                Base::PROVIDES_LOCAL_FRAME
+    };
+
+public:
+    // input
+    VectorType m_n_l0;
+    MatrixType m_P_l0;
+
+    // accumulation
+    MatrixType m_B_ijk[3];
+    Scalar m_deno;
+
+    // results
+    Scalar m_k1, m_k2;
+    VectorType m_dir1, m_dir2;
+
+protected:
+    static Scalar rho_der(Scalar t);
+    Mat32 tangentPlane() const;
+
+public:
+    PONCA_EXPLICIT_CAST_OPERATORS(Varifold,varifold)
+    void init(const VectorType& x_l0, const VectorType& n_l0);
+    PONCA_FITTING_DECLARE_ADDNEIGHBOR
+    PONCA_FITTING_DECLARE_FINALIZE
+
+    // plane
+    VectorType project(const VectorType& p) const;
+    VectorType primitiveGradient(const VectorType& p) const;
+
+};
+
+} //namespace v0
+
+// ============================================================================
+
+namespace v1
+{
+
+//! 
+//! Implementation of 
+//!     Weak and approximate curvatures of a measure: a varifold perspective
+//!     B Buet, GP Leonardi, S Masnou
+//!     
+//! See Equation 7.2 of the article
+//!
+//! The _WFunctor must be DistWeightFunc<D,VarifoldWeightKernel<S>>.
+//! Since rho' is negative and the basket ignores negative weights,
+//! VarifoldWeightKernel::f() returns the opposite (positive) weight
+//! and Varifold::addLocalNeighbor() takes the opposite back (see WARNING).
+//! 
+//! Remarks
+//! - point weights (m_l) are set to 1
+//! - only valid in 3D
+//! - normals are required
+//! 
+template<class DataPoint, class _WFunctor, typename T>
+class Varifold : public T
+{
+    PONCA_FITTING_DECLARE_DEFAULT_TYPES
+    PONCA_FITTING_DECLARE_MATRIX_TYPE
+
+protected:
+    using Mat32 = Eigen::Matrix<Scalar,3,2>;
+    using Mat22 = Eigen::Matrix<Scalar,2,2>;
+
+    static_assert(DataPoint::Dim == 3); // only valid in 3D
+    // force the use of VarifoldWeightKernel
+    static_assert(std::is_same<_WFunctor, DistWeightFunc<DataPoint,VarifoldWeightKernel<Scalar>>>());
+
+public:
+    // input
+    VectorType m_n_l0;
+    MatrixType m_P_l0;
+
+    // accumulation
+    MatrixType m_A;
+
+    // results
+    Scalar m_k1, m_k2;
+    VectorType m_dir1, m_dir2;
+
+    // plane
+    bool m_planeIsReady = false;
+    VectorType m_sumN; // sum of normals
+    Scalar     m_sumWeight; // sum of weights
+
+
+protected:
+    Mat32 tangentPlane() const;
+
+public:
+    PONCA_EXPLICIT_CAST_OPERATORS(Varifold,varifold)
+    void init(const VectorType& x_l0, const VectorType& n_l0);
+    void init(const VectorType& _evalPos);
+    PONCA_FITTING_DECLARE_ADDNEIGHBOR
+    PONCA_FITTING_DECLARE_FINALIZE
+
+    PONCA_MULTIARCH inline Scalar f_smooth  (const Scalar& _x) const { Scalar v = _x*_x - Scalar(1.); return v*v; }
+
+    // plane
+    VectorType project(const VectorType& p) const;
+    VectorType primitiveGradient (const VectorType& p) const;
+    VectorType primitiveGradient () const { return primitiveGradient(Base::m_w.basisCenter()); }
+
+
+
+    PONCA_MULTIARCH inline Scalar kmin () const { return m_k1; }
+    PONCA_MULTIARCH inline Scalar kmax () const { return m_k2; }
+    PONCA_MULTIARCH inline Scalar kMean () const { return (m_k1 + m_k2)/2; }
+    
+    
+    PONCA_MULTIARCH inline VectorType kminDirection() const { return m_dir1; }
+    PONCA_MULTIARCH inline VectorType kmaxDirection() const { return m_dir2; }
+
+};
+
+template<class _Scalar>
+class VarifoldWeightKernel
+{
+public:
+    using Scalar = _Scalar;
+
+    Scalar f(const Scalar& _x) const {
+        const Scalar y = Scalar(1) / (1 - _x*_x);
+        //
+        // WARNING
+        // rho prime is negative but the basket ignores negative weights
+        // (see Basket::addNeighbor()), so the opposite is returned here
+        // and Varifold::addLocalNeighbor() takes the opposite again
+        // 
+        // return - 2 * _x * y * y * std::exp(-y);
+        return + 2 * _x * y * y * std::exp(-y);
+    }
+};
+
+} //namespace v1
+} //namespace Ponca
+
+#include "varifold.hpp"
---
+++ Ponca/src/Fitting/varifold.hpp
@@ -0,0 +1,340 @@
+
+namespace Ponca
+{
+
+namespace v0 
+{
+
+template<class DataPoint, class _WFunctor, typename T>
+void Varifold<DataPoint, _WFunctor, T>::init(
+    const VectorType& x_l0,
+    const VectorType& n_l0)
+{
+    Base::init(x_l0);
+
+    m_n_l0 = n_l0;
+    m_P_l0 = MatrixType::Identity() - n_l0 * n_l0.transpose();
+
+    for(auto k = 0; k < 3; ++k)
+        m_B_ijk[k] = MatrixType::Zero();
+    m_deno = Scalar(0);
+
+    m_k1 = Scalar(0);
+    m_dir1 = VectorType::Zero();
+    m_k2 = Scalar(0);
+    m_dir2 = VectorType::Zero();
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+bool Varifold<DataPoint, _WFunctor, T>::addLocalNeighbor(
+    Scalar w, const VectorType &localQ, const DataPoint &attributes)
+{
+    if(not Base::addLocalNeighbor(w, localQ, attributes))
+        return false;
+
+    if(localQ.squaredNorm() < Eigen::NumTraits<Scalar>::dummy_precision())
+        return false;
+    
+    const Scalar t = Base::m_w.evalScale();
+
+    // x_l0 = 0 in centered basis 
+    const VectorType& x_l = localQ;
+    const VectorType x_l0 = VectorType::Zero();
+    const VectorType n_l = attributes.normal();
+    const MatrixType P_l = MatrixType::Identity() - n_l * n_l.transpose();
+    constexpr Scalar m_l = 1.0;
+    constexpr Scalar d = 2.0;
+    constexpr Scalar n = 3.0;
+
+    const Scalar rho_prime = rho_der((x_l - x_l0).norm() / t);
+    const Scalar xi = - t * rho_prime / 3;
+
+    const Scalar coeff = d/n 
+        * m_l
+        * rho_prime
+        / (x_l0 - x_l).norm()
+        * 1.0/2.0;
+
+    m_deno += m_l * xi;
+
+    const VectorType u = P_l * (x_l0 - x_l);
+    const MatrixType Pdiff = P_l - m_P_l0;
+
+    for(auto i = 0; i < 3; ++i)
+    {
+        VectorType e_i = VectorType::Zero();
+        e_i[i] = 1;
+        for(auto j = 0; j < 3; ++j)
+        {
+            VectorType e_j = VectorType::Zero();
+            e_j[j] = 1;
+            for(auto k = 0; k < 3; ++k)
+            {
+                VectorType e_k = VectorType::Zero();
+                e_k[k] = 1;
+                {
+                    const VectorType v1 = Pdiff(j,k) * e_i;
+                    const VectorType v2 = Pdiff(i,k) * e_j;
+                    const VectorType v3 = Pdiff(i,j) * e_k;
+                    const VectorType v = v1 + v2 - v3;
+
+                    m_B_ijk[k](i,j) += coeff * u.dot(v);
+                }
+            }
+        }
+    }
+    return true;
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+FIT_RESULT Varifold<DataPoint, _WFunctor, T>::finalize()
+{
+    if(Base::finalize() != STABLE)
+        return Base::m_eCurrentState; 
+
+    constexpr Scalar epsilon = Eigen::NumTraits<Scalar>::dummy_precision();
+
+    if(std::abs(m_deno) < epsilon)
+        return UNSTABLE;
+
+    for(auto k = 0; k < 3; ++k) {
+        m_B_ijk[k] /= m_deno;
+    }
+
+    MatrixType B_ij = MatrixType::Zero();
+    for(auto i = 0; i < 3; ++i)
+    for(auto j = 0; j < 3; ++j)
+    for(auto k = 0; k < 3; ++k)
+    {
+        B_ij(i,j) += m_B_ijk[k](i,j) * m_n_l0[k];
+    }
+
+    const Mat32 Q = tangentPlane();
+    const Mat22 B = - Q.transpose() * B_ij * Q; // minus for sign convention
+
+    Eigen::SelfAdjointEigenSolver<Mat22> solver;
+    solver.computeDirect(B);
+
+    if(solver.info() != Eigen::Success)
+        return UNSTABLE;
+
+    m_k1 = solver.eigenvalues()[0];
+    m_dir1 = Q * solver.eigenvectors().col(0);
+    m_k2 = solver.eigenvalues()[1];
+    m_dir2 = Q * solver.eigenvectors().col(1);
+
+    // if(std::abs(m_k1) < std::abs(m_k2))
+    // {
+    //     std::swap(m_k1, m_k1);
+    //     std::swap(m_dir1, m_dir2);
+    // }
+
+    return STABLE;
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+typename Varifold<DataPoint, _WFunctor, T>::Mat32 Varifold<DataPoint, _WFunctor, T>::tangentPlane() const
+{
+    Mat32 B;
+    int i0=-1, i1=-1, i2=-1;
+    const VectorType& n = m_n_l0;
+    n.array().abs().minCoeff(&i0); // i0: dimension where n extends the least
+    i1 = (i0+1)%3;
+    i2 = (i0+2)%3;
+
+    B.col(0)[i0] = 0;
+    B.col(0)[i1] = n[i2];
+    B.col(0)[i2] = -n[i1];
+
+    B.col(0).normalize();
+    B.col(1) = B.col(0).cross(n);
+    return B;
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+typename Varifold<DataPoint, _WFunctor, T>::Scalar Varifold<DataPoint, _WFunctor, T>::rho_der(Scalar _x)
+{
+    const Scalar g = Scalar(1) / (Scalar(1) - _x*_x);
+    return - Scalar(2) * _x  * g * g * std::exp(-g);
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+typename Varifold<DataPoint, _WFunctor, T>::VectorType Varifold<DataPoint, _WFunctor, T>::project(const VectorType& p) const
+{
+    return Base::m_w.basisCenter() + (p - (p - Base::m_w.basisCenter()).dot(m_n_l0) * m_n_l0);
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+typename Varifold<DataPoint, _WFunctor, T>::VectorType Varifold<DataPoint, _WFunctor, T>::primitiveGradient(const VectorType&) const
+{
+    return m_n_l0;
+}
+
+} //namespace v0
+
+// ============================================================================
+
+namespace v1
+{
+
+template<class DataPoint, class _WFunctor, typename T>
+void Varifold<DataPoint, _WFunctor, T>::init(
+    const VectorType& x_l0,
+    const VectorType& n_l0)
+{
+    Base::init(x_l0);
+
+    m_n_l0 = n_l0;
+    m_P_l0 = MatrixType::Identity() - n_l0 * n_l0.transpose();
+
+    m_A = MatrixType::Zero();
+
+    m_k1 = Scalar(0);
+    m_dir1 = VectorType::Zero();
+    m_k2 = Scalar(0);
+    m_dir2 = VectorType::Zero();
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+void Varifold<DataPoint, _WFunctor, T>::init(
+    const VectorType& _evalPos)
+{
+    Base::init(_evalPos);
+    m_sumN = VectorType::Zero();
+    m_sumWeight = Scalar(0);
+
+    m_n_l0 = VectorType::Zero();
+
+    m_A = MatrixType::Zero();
+
+    m_k1 = Scalar(0);
+    m_dir1 = VectorType::Zero();
+    m_k2 = Scalar(0);
+    m_dir2 = VectorType::Zero();
+
+    m_planeIsReady = false;
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+bool Varifold<DataPoint, _WFunctor, T>::addLocalNeighbor(
+    Scalar w, const VectorType &localQ, const DataPoint &attributes)
+{
+    // auto res = Base::addLocalNeighbor(w, localQ, attributes);
+    if (! m_planeIsReady)
+    {
+        VectorType baryCenter = Base::m_w.basisCenter();
+        VectorType pos = attributes.pos();
+        VectorType centerPos = pos - baryCenter;
+        Scalar dist = centerPos.norm();
+        Scalar val = f_smooth(dist / Base::m_w.evalScale());
+
+        m_sumN += val * attributes.normal();
+        m_sumWeight += val;
+
+        return true;
+    }
+    else {
+        if(not Base::addLocalNeighbor(w, localQ, attributes))
+            return false;
+
+        if(localQ.squaredNorm() < Eigen::NumTraits<Scalar>::dummy_precision())
+            return false;
+
+        const VectorType n_l = attributes.normal();
+        const MatrixType P_l = MatrixType::Identity() - n_l * n_l.transpose();
+        const VectorType delta_x = - localQ.normalized();
+        const VectorType u = P_l * delta_x;
+        const MatrixType DeltaP = P_l - m_P_l0;
+        const VectorType Pl_nl0 = P_l * m_n_l0;
+
+        const MatrixType a1 = u * Pl_nl0.transpose();
+        const MatrixType a2 = u.dot(m_n_l0) * DeltaP;
+
+        w *= -1; // see WARNING in VarifoldWeightKernel::f()
+
+        m_A += w * (a1 + a1.transpose() - a2);
+    }
+    
+    return true;
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+FIT_RESULT Varifold<DataPoint, _WFunctor, T>::finalize()
+{
+
+    if ( ! m_planeIsReady ){
+        m_planeIsReady = true;
+        m_n_l0 = m_sumN / m_sumWeight;
+        m_P_l0 = MatrixType::Identity() - m_n_l0 * m_n_l0.transpose();
+        return Base::m_eCurrentState = NEED_OTHER_PASS;
+    }
+    else {
+
+        // if(Base::finalize() != STABLE)
+        //     return Base::m_eCurrentState;
+        constexpr Scalar epsilon = Eigen::NumTraits<Scalar>::dummy_precision();
+
+        Scalar sum_weight = Base::getWeightSum();
+
+        if(std::abs(sum_weight) < epsilon)
+            return UNSTABLE;
+
+        m_A /= sum_weight;
+
+        const Mat32 Q = tangentPlane();
+        const Mat22 B = - Q.transpose() * m_A * Q; // minus for sign convention
+
+        Eigen::SelfAdjointEigenSolver<Mat22> solver;
+        solver.computeDirect(B);
+
+        if(solver.info() != Eigen::Success)
+            return UNSTABLE;
+
+        m_k1 = solver.eigenvalues()[0];
+        m_dir1 = Q * solver.eigenvectors().col(0);
+        m_k2 = solver.eigenvalues()[1];
+        m_dir2 = Q * solver.eigenvectors().col(1);
+
+        if (m_k1 > m_k2)
+        {
+            std::swap(m_k1, m_k2);
+            std::swap(m_dir1, m_dir2);
+        }
+    }
+
+    return STABLE;
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+typename Varifold<DataPoint, _WFunctor, T>::Mat32 Varifold<DataPoint, _WFunctor, T>::tangentPlane() const
+{
+    Mat32 B;
+    int i0=-1, i1=-1, i2=-1;
+    const VectorType& n = m_n_l0;
+    n.array().abs().minCoeff(&i0); // i0: dimension where n extends the least
+    i1 = (i0+1)%3;
+    i2 = (i0+2)%3;
+
+    B.col(0)[i0] = 0;
+    B.col(0)[i1] = n[i2];
+    B.col(0)[i2] = -n[i1];
+
+    B.col(0).normalize();
+    B.col(1) = B.col(0).cross(n);
+    return B;
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+typename Varifold<DataPoint, _WFunctor, T>::VectorType Varifold<DataPoint, _WFunctor, T>::project(const VectorType& p) const
+{
+    return Base::m_w.basisCenter() + (p - (p - Base::m_w.basisCenter()).dot(m_n_l0) * m_n_l0);
+}
+
+template<class DataPoint, class _WFunctor, typename T>
+typename Varifold<DataPoint, _WFunctor, T>::VectorType Varifold<DataPoint, _WFunctor, T>::primitiveGradient(const VectorType&) const
+{
+    return m_n_l0;
+}
+
+} //namespace v1
+} //namespace Ponca

dba11dbcedadebc283ba82853572bf7644faf0bc
Leo <arnalleouis@gmail.com>
2024-01-22 14:06:44 +0100

    Temporary way to add meanPlaneDer

--- Ponca/src/Fitting/meanPlaneFit.h
+++ Ponca/src/Fitting/meanPlaneFit.h
@@ -83,4 +83,75 @@ public:
                     Plane<DataPoint, _WFunctor,T>>>>>;
 //! [MeanPlaneFit Definition]
 
+/*!
+    \brief Internal generic class computing the derivatives of mean plane fits
+    \inherit Concept::FittingProcedureConcept
+
+    \see Plane
+    \see localFrame
+*/
+template < class DataPoint, class _WFunctor, int DiffType, typename T>
+class MeanPlaneDerImpl : public T
+{
+    PONCA_FITTING_DECLARE_DEFAULT_TYPES
+    PONCA_FITTING_DECLARE_MATRIX_TYPE
+    PONCA_FITTING_DECLARE_DEFAULT_DER_TYPES
+
+protected:
+    enum { Check = Base::PROVIDES_PLANE
+                && Base::PROVIDES_MEAN_NORMAL_DERIVATIVE
+                && Base::PROVIDES_MEAN_POSITION_DERIVATIVE, 
+                PROVIDES_MEAN_PLANE_DERIVATIVE,                    /*!< \brief Provides derivatives for hyper-planes */
+                PROVIDES_NORMAL_DERIVATIVE
+         };
+
+private:    
+    VectorArray m_dNormal {VectorArray::Zero()};    /*!< \brief Derivatives of the hyper-plane normal */
+    ScalarArray m_dDist {ScalarArray::Zero()};      /*!< \brief Derivatives of the MLS scalar field */
+
+
+public:
+    PONCA_EXPLICIT_CAST_OPERATORS_DER(MeanPlaneDerImpl,meanPlaneDer)
+
+    PONCA_MULTIARCH inline FIT_RESULT finalize(){
+        Base::finalize();
+        // Test if base finalize end on a viable case (stable / unstable)
+        if (this->isReady())
+        {
+            VectorType   barycenter = Base::barycenter();
+            VectorArray dBarycenter = Base::barycenterDerivatives();
+
+            VectorType normal = Base::primitiveGradient();
+            m_dNormal = Base::m_dSumN / Base::getWeightSum();
+
+            for (int k = 0; k < DataPoint::Dim; ++k){
+                VectorType dDiff = dBarycenter.col(k);
+                if (k > 0 || !Base::isScaleDer())
+                    dDiff(Base::isScaleDer() ? k - 1 : k) += 1;
+                m_dDist(k) = m_dNormal.col(k).dot(barycenter) + normal.dot(dDiff);
+            }
+            
+            // \fixme we shouldn't need this normalization, however currently the derivatives are overestimated by a factor 2
+            // m_dNormal /= Scalar(2.);
+        }
+        return Base::m_eCurrentState;
+    }
+
+    /*! \brief Returns the derivatives of the scalar field at the evaluation point */
+    PONCA_MULTIARCH inline ScalarArray dPotential() const { return m_dDist; }
+
+    /*! \brief Returns the derivatives of the primitive normal */
+    PONCA_MULTIARCH inline VectorArray dNormal() const { return m_dNormal; }
+
+}; //class MeanPlaneFitImpl
+
+/// \brief Helper alias for Plane fitting on points using MeanPlaneDerImpl
+//! [MeanPlaneDer Definition]
+    template < class DataPoint, class _WFunctor, int DiffType, typename T>
+    using MeanPlaneDer =
+        MeanPlaneDerImpl<DataPoint, _WFunctor, DiffType,
+            MeanNormalDer<DataPoint, _WFunctor, DiffType,
+                MeanPositionDer<DataPoint, _WFunctor, DiffType, T>>>;
+//! [MeanPlaneDer Definition]
+
 } //namespace Ponca
